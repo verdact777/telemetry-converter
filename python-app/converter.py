b93f6db9bf3f0a1c27d8d4045ef78473e658ca7a
Adwaith praveen <24bcla02@kristujayanti.com>
2026-05-15 20:37:03 +0530

    Add python-app/converter.py

---
+++ python-app/converter.py
@@ -0,0 +1,46 @@
+import json, datetime
+
+
+def convertFromFormat1(jsonObject):
+    locationParts = jsonObject["location"].split("/")
+    return {
+        'deviceID': jsonObject['deviceID'],
+        'deviceType': jsonObject['deviceType'],
+        'timestamp': jsonObject['timestamp'],
+        'location': {
+            'country': locationParts[0],
+            'city': locationParts[1],
+            'area': locationParts[2],
+            'factory': locationParts[3],
+            'section': locationParts[4]
+        },
+        'data': {
+            'status': jsonObject['operationStatus'],
+            'temperature': jsonObject['temp']
+        }
+    }
+
+
+def convertFromFormat2(jsonObject):
+    data = datetime.datetime.strptime(jsonObject['timestamp'], '%Y-%m-%dT%H:%M:%S.%fZ')
+    timestamp = round((data - datetime.datetime(1970, 1, 1)).total_seconds() * 1000)
+    return {
+        'deviceID': jsonObject['device']['id'],
+        'deviceType': jsonObject['device']['type'],
+        'timestamp': timestamp,
+        'location': {
+            'country': jsonObject['country'],
+            'city': jsonObject['city'],
+            'area': jsonObject['area'],
+            'factory': jsonObject['factory'],
+            'section': jsonObject['section']
+        },
+        'data': jsonObject['data']
+    }
+
+
+def main(jsonObject):
+    if jsonObject.get('device') is None:
+        return convertFromFormat1(jsonObject)
+    else:
+        return convertFromFormat2(jsonObject)
